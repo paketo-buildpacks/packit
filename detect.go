56c50175eb43b4c793fe1799dacb9515e7bd4298
Frankie Gallina-Jones <frankieg@vmware.com>
2020-08-27 15:11:29 +0000

    Uses CNB_BUILDPACK_DIR env variable to set CNBPath
    The old strategy is used if it is not available
    
    Implements Buildpack API version 0.3
    
    Signed-off-by: Timothy Hitchener <thitchener@pivotal.io>

--- detect.go
+++ detect.go
@@ -106,7 +106,10 @@ func Detect(f DetectFunc, options ...Option) {
 		return
 	}
 
-	cnbPath := filepath.Clean(strings.TrimSuffix(config.args[0], filepath.Join("bin", "detect")))
+	cnbPath := os.Getenv("CNB_BUILDPACK_DIR")
+	if cnbPath == "" {
+		cnbPath = filepath.Clean(strings.TrimSuffix(config.args[0], filepath.Join("bin", "detect")))
+	}
 
 	var buildpackInfo struct {
 		Buildpack BuildpackInfo `toml:"buildpack"`
