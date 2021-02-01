d0a89f1fe0bef290dfb5680a61eb2dec86296570
Sophie Wigmore <swigmore@vmware.com>
2021-02-01 11:05:24 -0500

    Use a fake for the mapping resolver
    - This might not be the best solution
    - Need to figure out how to fake out the /platform/binding path

--- postal/dependency_mappings.go
+++ postal/dependency_mappings.go
@@ -8,49 +8,44 @@ import (
 	"strings"
 )
 
-type DependencyMapping struct {
-	SHA256 string
-	URI    string
-}
+type DependencyMappingResolver struct{}
 
-func NewDependencyMapping(sha256, uri string) DependencyMapping {
-	return DependencyMapping{
-		SHA256: sha256,
-		URI:    uri,
-	}
+func NewDependencyMappingResolver() DependencyMappingResolver {
+	return DependencyMappingResolver{}
 }
 
 // Given a target dependency, look up if there is a matching dependency mapping at the given binding path
-func (d Dependency) FindDependencyMapping(bindingPath string) (DependencyMapping, error) {
+func (d DependencyMappingResolver) FindDependencyMapping(sha256, bindingPath string) (string, error) {
+	fmt.Println(bindingPath)
 	allBindings, err := filepath.Glob(filepath.Join(bindingPath, "*"))
 	if err != nil {
-		return NewDependencyMapping("", ""), err
+		return "", err
 	}
 
 	for _, binding := range allBindings {
 		// check if any of the bindings are of type "dependency-mapping"
 		bindType, err := ioutil.ReadFile(filepath.Join(binding, "type"))
 		if err != nil {
-			return NewDependencyMapping("", ""), fmt.Errorf("couldn't read binding type: %w", err)
+			return "", fmt.Errorf("couldn't read binding type: %w", err)
 		}
 
 		// if it is a dependency mapping, look for a SHA match
 		if strings.Contains(string(bindType), "dependency-mapping") {
-			if _, err := os.Stat(filepath.Join(binding, d.SHA256)); err != nil {
+			if _, err := os.Stat(filepath.Join(binding, sha256)); err != nil {
 				if !os.IsNotExist(err) {
-					return NewDependencyMapping("", ""), err
+					return "", err
 				}
 				continue
 			}
 
 			// A matching SHA256 has been found, return the associated URI
-			uri, err := ioutil.ReadFile(filepath.Join(binding, d.SHA256))
+			uri, err := ioutil.ReadFile(filepath.Join(binding, sha256))
 			if err != nil {
-				return NewDependencyMapping("", ""), err
+				return "", err
 			}
-			dependency := NewDependencyMapping(d.SHA256, strings.TrimSuffix(string(uri), "\n"))
-			return dependency, nil
+			fmt.Println("got the good uri")
+			return strings.TrimSuffix(string(uri), "\n"), nil
 		}
 	}
-	return NewDependencyMapping("", ""), nil
+	return "", nil
 }
