31c3a91d303b58cc21d53e80095adeb40f1b17dc
Forest Eckhardt <feckhardt@pivotal.io>
2021-02-03 17:36:42 +0000

    Finishes work to update build image version
    - Also finishes unhappy path tests

--- cargo/jam/internal/image_test.go
+++ cargo/jam/internal/image_test.go
@@ -113,5 +113,129 @@ func testImage(t *testing.T, context spec.G, it spec.S) {
 				})
 			})
 		})
-	})
+	}, spec.Sequential())
+
+	context("FindLatestBuildImage", func() {
+		it.Before(func() {
+			server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				if req.Header.Get("Authorization") != "Basic c29tZS11c2VybmFtZTpzb21lLXBhc3N3b3Jk" {
+					w.Header().Set("WWW-Authenticate", `Basic realm="localhost"`)
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
+
+				switch req.URL.Path {
+				case "/v2/":
+					w.WriteHeader(http.StatusOK)
+
+				case "/v2/some-org/some-repo-build/tags/list":
+					w.WriteHeader(http.StatusOK)
+					fmt.Fprintln(w, `{
+						  "tags": [
+								"0.0.10-some-cnb",
+								"0.20.1",
+								"0.20.12-some-cnb",
+								"0.20.12-other-cnb",
+								"latest"
+							]
+					}`)
+
+				case "/v2/some-org/error-repo/tags/list":
+					w.WriteHeader(http.StatusTeapot)
+
+				default:
+					t.Fatal(fmt.Sprintf("unknown path: %s", req.URL.Path))
+				}
+			}))
+
+			var err error
+			dockerConfig, err = ioutil.TempDir("", "docker-config")
+			Expect(err).NotTo(HaveOccurred())
+
+			contents := fmt.Sprintf(`{
+				"auths": {
+					%q: {
+						"username": "some-username",
+						"password": "some-password"
+					}
+				}
+			}`, strings.TrimPrefix(server.URL, "http://"))
+
+			err = ioutil.WriteFile(filepath.Join(dockerConfig, "config.json"), []byte(contents), 0600)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(os.Setenv("DOCKER_CONFIG", dockerConfig)).To(Succeed())
+		})
+
+		it.After(func() {
+			Expect(os.Unsetenv("DOCKER_CONFIG")).To(Succeed())
+			Expect(os.RemoveAll(dockerConfig)).To(Succeed())
+		})
+
+		it("returns the latest semver tag for the given image uri", func() {
+			image, err := internal.FindLatestBuildImage(
+				fmt.Sprintf("%s/some-org/some-repo-run:some-cnb", strings.TrimPrefix(server.URL, "http://")),
+				fmt.Sprintf("%s/some-org/some-repo-build:0.0.10-some-cnb", strings.TrimPrefix(server.URL, "http://")),
+			)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(image).To(Equal(internal.Image{
+				Name:    fmt.Sprintf("%s/some-org/some-repo-build", strings.TrimPrefix(server.URL, "http://")),
+				Path:    "some-org/some-repo-build",
+				Version: "0.20.12-some-cnb",
+			}))
+		})
+
+		context("failure cases", func() {
+			context("when the run uri cannot be parsed", func() {
+				it("returns an error", func() {
+					_, err := internal.FindLatestBuildImage(
+						"not a valid uri",
+						fmt.Sprintf("%s/some-org/some-repo-build:0.0.10-some-cnb", strings.TrimPrefix(server.URL, "http://")),
+					)
+					Expect(err).To(MatchError("failed to parse run image reference \"not a valid uri\": invalid reference format"))
+				})
+			})
+
+			context("when the run image is not tagged", func() {
+				it("returns an error", func() {
+					_, err := internal.FindLatestBuildImage(
+						fmt.Sprintf("%s/some-org/some-repo-run", strings.TrimPrefix(server.URL, "http://")),
+						fmt.Sprintf("%s/some-org/some-repo-build:0.0.10-some-cnb", strings.TrimPrefix(server.URL, "http://")),
+					)
+					Expect(err).To(MatchError("expected the run image to be tagged but it was not"))
+				})
+			})
+
+			context("when the build uri cannot be parsed", func() {
+				it("returns an error", func() {
+					_, err := internal.FindLatestBuildImage(
+						fmt.Sprintf("%s/some-org/some-repo-run:some-cnb", strings.TrimPrefix(server.URL, "http://")),
+						"not a valid uri",
+					)
+					Expect(err).To(MatchError("failed to parse build image reference \"not a valid uri\": invalid reference format"))
+				})
+			})
+
+			context("when the repo name cannot be parsed", func() {
+				it("returns an error", func() {
+					_, err := internal.FindLatestBuildImage(
+						fmt.Sprintf("%s/some-org/some-repo-run:some-cnb", strings.TrimPrefix(server.URL, "http://")),
+						fmt.Sprintf("%s/a:latest", strings.TrimPrefix(server.URL, "http://")),
+					)
+					Expect(err).To(MatchError("failed to parse build image repository: repository must be between 2 and 255 runes in length: a"))
+				})
+			})
+
+			context("when the tags cannot be listed", func() {
+				it("returns an error", func() {
+					_, err := internal.FindLatestBuildImage(
+						fmt.Sprintf("%s/some-org/some-repo-run:some-cnb", strings.TrimPrefix(server.URL, "http://")),
+						fmt.Sprintf("%s/some-org/error-repo:latest", strings.TrimPrefix(server.URL, "http://")),
+					)
+					Expect(err).To(MatchError(ContainSubstring("failed to list tags:")))
+					Expect(err).To(MatchError(ContainSubstring("status code 418")))
+				})
+			})
+		})
+	}, spec.Sequential())
 }
--- cargo/jam/update_builder_test.go
+++ cargo/jam/update_builder_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -74,7 +75,25 @@ func testUpdateBuilder(t *testing.T, context spec.G, it spec.S) {
 							]
 					}`)
 
+			case "/v2/somerepository/build/tags/list":
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprintln(w, `{
+						  "tags": [
+								"0.0.10-some-cnb",
+								"0.20.1",
+								"0.20.12-some-cnb",
+								"0.20.12-other-cnb",
+								"latest"
+							]
+					}`)
+
 			case "/v2/some-repository/error-buildpack-id/tags/list":
+				w.WriteHeader(http.StatusTeapot)
+
+			case "/v2/some-repository/error-lifecycle/tags/list":
+				w.WriteHeader(http.StatusTeapot)
+
+			case "/v2/somerepository/error-build/tags/list":
 				w.WriteHeader(http.StatusTeapot)
 
 			default:
@@ -114,8 +133,8 @@ description = "Some description"
 
 [stack]
   id = "io.paketo.stacks.some-stack"
-  build-image = "REGISTRY-URI/somerepository/build:1.2.3-some-cnb"
-  run-image = "REGISTRY-URI.io/somerepository/run:some-cnb"
+  build-image = "REGISTRY-URI/somerepository/build:0.0.10-some-cnb"
+  run-image = "REGISTRY-URI/somerepository/run:some-cnb"
   run-image-mirrors = ["REGISTRY-URI/some-repository/run:some-cnb"]
 			`), []byte(`REGISTRY-URI`), []byte(strings.TrimPrefix(server.URL, "http://"))), 0600)
 		Expect(err).NotTo(HaveOccurred())
@@ -130,7 +149,7 @@ description = "Some description"
 			path,
 			"update-builder",
 			"--builder-file", filepath.Join(builderDir, "builder.toml"),
-			"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", server.URL),
+			"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", strings.TrimPrefix(server.URL, "http://")),
 		)
 
 		buffer := gbytes.NewBuffer()
@@ -169,9 +188,168 @@ description = "Some description"
 
 [stack]
   id = "io.paketo.stacks.some-stack"
-  build-image = "REGISTRY-URI/somerepository/build:1.2.3-some-cnb"
-  run-image = "REGISTRY-URI.io/somerepository/run:some-cnb"
+  build-image = "REGISTRY-URI/somerepository/build:0.20.12-some-cnb"
+  run-image = "REGISTRY-URI/somerepository/run:some-cnb"
   run-image-mirrors = ["REGISTRY-URI/some-repository/run:some-cnb"]
 			`, "REGISTRY-URI", strings.TrimPrefix(server.URL, "http://"))))
+	})
+
+	context("failure cases", func() {
+		context("when the --builder-file flag is missing", func() {
+			it("prints an error and exits non-zero", func() {
+				command := exec.Command(
+					path,
+					"update-builder",
+					"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", strings.TrimPrefix(server.URL, "http://")),
+				)
+
+				buffer := gbytes.NewBuffer()
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(1), func() string { return string(buffer.Contents()) })
+				Expect(string(buffer.Contents())).To(Equal("failed to execute: --builder-file is a required flag"))
+			})
+		})
+
+		context("when the builder file cannot be found", func() {
+			it("prints an error and exits non-zero", func() {
+				command := exec.Command(
+					path,
+					"update-builder",
+					"--builder-file", "/no/such/file",
+					"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", strings.TrimPrefix(server.URL, "http://")),
+				)
+
+				buffer := gbytes.NewBuffer()
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(1), func() string { return string(buffer.Contents()) })
+				Expect(string(buffer.Contents())).To(Equal("failed to execute: failed to open builder config file: open /no/such/file: no such file or directory"))
+			})
+		})
+
+		context("when the latest buildpack image cannot be found", func() {
+			it.Before(func() {
+				err := ioutil.WriteFile(filepath.Join(builderDir, "builder.toml"), bytes.ReplaceAll([]byte(`
+[[buildpacks]]
+	uri = "docker://REGISTRY-URI/some-repository/error-buildpack-id:0.0.10"
+  version = "0.0.10"
+			`), []byte(`REGISTRY-URI`), []byte(strings.TrimPrefix(server.URL, "http://"))), 0600)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			it("prints an error and exits non-zero", func() {
+				command := exec.Command(
+					path,
+					"update-builder",
+					"--builder-file", filepath.Join(builderDir, "builder.toml"),
+					"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", strings.TrimPrefix(server.URL, "http://")),
+				)
+
+				buffer := gbytes.NewBuffer()
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(1), func() string { return string(buffer.Contents()) })
+				Expect(string(buffer.Contents())).To(ContainSubstring("failed to list tags"))
+			})
+		})
+
+		context("when the latest lifecycle image cannot be found", func() {
+			it("prints an error and exits non-zero", func() {
+				command := exec.Command(
+					path,
+					"update-builder",
+					"--builder-file", filepath.Join(builderDir, "builder.toml"),
+					"--lifecycle-uri", fmt.Sprintf("%s/some-repository/error-lifecycle", strings.TrimPrefix(server.URL, "http://")),
+				)
+
+				buffer := gbytes.NewBuffer()
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(1), func() string { return string(buffer.Contents()) })
+				Expect(string(buffer.Contents())).To(ContainSubstring("failed to list tags"))
+			})
+		})
+
+		context("when the latest build image cannot be found", func() {
+			it.Before(func() {
+				err := ioutil.WriteFile(filepath.Join(builderDir, "builder.toml"), bytes.ReplaceAll([]byte(`
+description = "Some description"
+
+[[buildpacks]]
+	uri = "docker://REGISTRY-URI/some-repository/some-buildpack-id:0.0.10"
+  version = "0.0.10"
+
+[[buildpacks]]
+	uri = "docker://REGISTRY-URI/some-repository/other-buildpack-id:0.20.22"
+  version = "0.20.22"
+
+[lifecycle]
+  version = "0.10.2"
+
+[[order]]
+
+  [[order.group]]
+    id = "some-repository/other-buildpack-id"
+    version = "0.20.22"
+
+[[order]]
+
+  [[order.group]]
+    id = "some-repository/some-buildpack-id"
+    version = "0.0.10"
+
+[stack]
+  id = "io.paketo.stacks.some-stack"
+	build-image = "REGISTRY-URI/somerepository/error-build:0.0.10-some-cnb"
+  run-image = "REGISTRY-URI/somerepository/run:some-cnb"
+  run-image-mirrors = ["REGISTRY-URI/some-repository/run:some-cnb"]
+			`), []byte(`REGISTRY-URI`), []byte(strings.TrimPrefix(server.URL, "http://"))), 0600)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			it("prints an error and exits non-zero", func() {
+				command := exec.Command(
+					path,
+					"update-builder",
+					"--builder-file", filepath.Join(builderDir, "builder.toml"),
+					"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", strings.TrimPrefix(server.URL, "http://")),
+				)
+
+				buffer := gbytes.NewBuffer()
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(1), func() string { return string(buffer.Contents()) })
+				Expect(string(buffer.Contents())).To(ContainSubstring("failed to list tags"))
+			})
+		})
+
+		context("when the builder file cannot be overwritten", func() {
+			it.Before(func() {
+				err := os.Chmod(filepath.Join(builderDir, "builder.toml"), 0400)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			it("prints an error and exits non-zero", func() {
+				command := exec.Command(
+					path,
+					"update-builder",
+					"--builder-file", filepath.Join(builderDir, "builder.toml"),
+					"--lifecycle-uri", fmt.Sprintf("%s/some-repository/lifecycle", strings.TrimPrefix(server.URL, "http://")),
+				)
+
+				buffer := gbytes.NewBuffer()
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(1), func() string { return string(buffer.Contents()) })
+				Expect(string(buffer.Contents())).To(ContainSubstring("failed to open builder config"))
+			})
+		})
 	})
 }
