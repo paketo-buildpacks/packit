fa4773a737c35c1bc60bc0281bbd0c426ff15ca7
Sophie Wigmore <swigmore@vmware.com>
2021-08-26 13:57:45 -0400

    remove algorithmName type

--- bom.go
+++ bom.go
@@ -44,7 +44,7 @@ type BOMSource struct {
 }
 
 type checksumAlgorithm struct {
-	name algorithmName
+	name string
 }
 
 // GetBOMChecksumAlgorithm takes in an algorithm string, and reasonably tries
@@ -53,39 +53,16 @@ type checksumAlgorithm struct {
 // Supported formats:
 // { 'MD5'| 'SHA-1'| 'SHA-256'| 'SHA-384'| 'SHA-512'| 'SHA3-256'| 'SHA3-384'| 'SHA3-512'| 'BLAKE2b-256'| 'BLAKE2b-384'| 'BLAKE2b-512'| 'BLAKE3'}
 func GetBOMChecksumAlgorithm(alg string) (checksumAlgorithm, error) {
-	switch {
-	case strings.EqualFold(alg, "SHA-256") || strings.EqualFold(alg, "SHA256"):
-		return checksumAlgorithm{name: SHA256}, nil
-	case strings.EqualFold(alg, "SHA-256") || strings.EqualFold(alg, "SHA256"):
-		return checksumAlgorithm{name: SHA256}, nil
-	case strings.EqualFold(alg, "SHA-1") || strings.EqualFold(alg, "SHA1"):
-		return checksumAlgorithm{name: SHA1}, nil
-	case strings.EqualFold(alg, "SHA-384") || strings.EqualFold(alg, "SHA384"):
-		return checksumAlgorithm{name: SHA384}, nil
-	case strings.EqualFold(alg, "SHA-512") || strings.EqualFold(alg, "SHA512"):
-		return checksumAlgorithm{name: SHA512}, nil
-	case strings.EqualFold(alg, "SHA3-256") || strings.EqualFold(alg, "SHA3256"):
-		return checksumAlgorithm{name: SHA3256}, nil
-	case strings.EqualFold(alg, "SHA3-384") || strings.EqualFold(alg, "SHA3384"):
-		return checksumAlgorithm{name: SHA3384}, nil
-	case strings.EqualFold(alg, "SHA3-512") || strings.EqualFold(alg, "SHA3512"):
-		return checksumAlgorithm{name: SHA3512}, nil
-	case strings.EqualFold(alg, "BLAKE2b-256") || strings.EqualFold(alg, "BLAKE2b256"):
-		return checksumAlgorithm{name: BLAKE2B256}, nil
-	case strings.EqualFold(alg, "BLAKE2b-384") || strings.EqualFold(alg, "BLAKE2b384"):
-		return checksumAlgorithm{name: BLAKE2B384}, nil
-	case strings.EqualFold(alg, "BLAKE2b-512") || strings.EqualFold(alg, "BLAKE2b512"):
-		return checksumAlgorithm{name: BLAKE2B512}, nil
-	case strings.EqualFold(alg, "BLAKE3") || strings.EqualFold(alg, "BLAKE-3"):
-		return checksumAlgorithm{name: BLAKE3}, nil
-	case strings.EqualFold(alg, "MD5"):
-		return checksumAlgorithm{name: MD5}, nil
+	for _, a := range []string{SHA256, SHA1, SHA384, SHA512, SHA3256, SHA3384, SHA3512, BLAKE2B256, BLAKE2B384, BLAKE2B512, BLAKE3, MD5} {
+		if strings.EqualFold(a, alg) || strings.EqualFold(strings.ReplaceAll(a, "-", ""), alg) {
+			return checksumAlgorithm{name: a}, nil
+		}
 	}
 
 	return checksumAlgorithm{}, fmt.Errorf("failed to get supported BOM checksum algorithm: %s is not valid", alg)
 }
 
-func (a checksumAlgorithm) String() algorithmName {
+func (a checksumAlgorithm) String() string {
 	return a.name
 }
 
@@ -93,21 +70,19 @@ func (a checksumAlgorithm) MarshalText() ([]byte, error) {
 	return []byte(a.String()), nil
 }
 
-type algorithmName string
-
 const (
-	SHA256     algorithmName = "SHA-256"
-	SHA1       algorithmName = "SHA-1"
-	SHA384     algorithmName = "SHA-384"
-	SHA512     algorithmName = "SHA-512"
-	SHA3256    algorithmName = "SHA3-256"
-	SHA3384    algorithmName = "SHA3-384"
-	SHA3512    algorithmName = "SHA3-512"
-	BLAKE2B256 algorithmName = "BLAKE2b-256"
-	BLAKE2B384 algorithmName = "BLAKE2b-384"
-	BLAKE2B512 algorithmName = "BLAKE2b-512"
-	BLAKE3     algorithmName = "BLAKE3"
-	MD5        algorithmName = "MD5"
+	SHA256     = "SHA-256"
+	SHA1       = "SHA-1"
+	SHA384     = "SHA-384"
+	SHA512     = "SHA-512"
+	SHA3256    = "SHA3-256"
+	SHA3384    = "SHA3-384"
+	SHA3512    = "SHA3-512"
+	BLAKE2B256 = "BLAKE2b-256"
+	BLAKE2B384 = "BLAKE2b-384"
+	BLAKE2B512 = "BLAKE2b-512"
+	BLAKE3     = "BLAKE3"
+	MD5        = "MD5"
 )
 
 // UnmetEntry contains the name of an unmet dependency from the build process
