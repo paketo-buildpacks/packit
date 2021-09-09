9dbcc2e2e2e9acc37819d9d99c86f4076ccb09ec
Ryan Moran <rmoran@vmware.com>
2021-09-09 15:04:25 -0700

    Extract out symlink sorting logic

--- vacation/zip_archive.go
+++ vacation/zip_archive.go
@@ -23,14 +23,6 @@ func NewZipArchive(inputReader io.Reader) ZipArchive {
 // Decompress reads from ZipArchive and writes files into the destination
 // specified.
 func (z ZipArchive) Decompress(destination string) error {
-	// Struct and slice to collect symlinks and create them after all files have
-	// been created
-	type header struct {
-		linkname string
-		path     string
-	}
-
-	var symlinkHeaders []header
 
 	// Use an os.File to buffer the zip contents. This is needed because
 	// zip.NewReader requires an io.ReaderAt so that it can jump around within
@@ -51,6 +43,7 @@ func (z ZipArchive) Decompress(destination string) error {
 		return fmt.Errorf("failed to create zip reader: %w", err)
 	}
 
+	var symlinks []symlink
 	for _, f := range zr.File {
 		// Clean the name in the header to prevent './filename' being stripped to
 		// 'filename' also to skip if the destination it the destination directory
@@ -94,9 +87,9 @@ func (z ZipArchive) Decompress(destination string) error {
 
 			// Collect all of the headers for symlinks so that they can be verified
 			// after all other files are written
-			symlinkHeaders = append(symlinkHeaders, header{
-				linkname: string(linkname),
-				path:     path,
+			symlinks = append(symlinks, symlink{
+				name: string(linkname),
+				path: path,
 			})
 
 		default:
@@ -130,69 +123,22 @@ func (z ZipArchive) Decompress(destination string) error {
 		}
 	}
 
-	// Create a map of all of the symlink names and where they are pointing to to
-	// act as a quasi-graph
-	symlinkMap := map[string]string{}
-	for _, h := range symlinkHeaders {
-		symlinkMap[filepath.Clean(h.path)] = h.linkname
+	symlinks, err = sortSymlinks(symlinks)
+	if err != nil {
+		return err
 	}
 
-	// Iterate over the symlink map for every link that is found this ensures
-	// that all symlinks that can be created will be created and any that are
-	// left over are cyclically dependent
-	maxIterations := len(symlinkMap)
-	for i := 0; i < maxIterations; i++ {
-		for path, linkname := range symlinkMap {
-			// Check to see if the linkname lies on the path of another symlink in
-			// the table or if it is another symlink in the table
-			//
-			// Example:
-			// path = dir/file
-			// a-symlink -> dir
-			// b-symlink -> a-symlink
-			// c-symlink -> a-symlink/file
-			//
-			// If there is a match either of the symlink or it is on the path then
-			// skip the creation of this symlink for now
-			shouldSkipLink := func() bool {
-				sln := strings.Split(linkname, "/")
-				for j := 0; j < len(sln); j++ {
-					if _, ok := symlinkMap[linknameFullPath(path, filepath.Join(sln[:j+1]...))]; ok {
-						return true
-					}
-				}
-				return false
-			}
-
-			if shouldSkipLink() {
-				continue
-			}
-
-			// If the linkname is not an existing link in the symlink table then we
-			// can attempt the make the link
-
-			// Check to see if the file that will be linked to is valid for symlinking
-			_, err := filepath.EvalSymlinks(linknameFullPath(path, linkname))
-			if err != nil {
-				return fmt.Errorf("failed to evaluate symlink %s: %w", path, err)
-			}
-
-			// Create the symlink
-			err = os.Symlink(linkname, path)
-			if err != nil {
-				return fmt.Errorf("failed to unzip symlink: %s", err)
-			}
-
-			// Remove the created symlink from the symlink table so that its
-			// dependent symlinks can be created in the next iteration
-			delete(symlinkMap, path)
+	for _, link := range symlinks {
+		// Check to see if the file that will be linked to is valid for symlinking
+		_, err := filepath.EvalSymlinks(linknameFullPath(link.path, link.name))
+		if err != nil {
+			return fmt.Errorf("failed to evaluate symlink %s: %w", link.path, err)
 		}
-	}
 
-	// Check to see if there are any symlinks left in the map which would
-	// indicate a cyclical dependency
-	if len(symlinkMap) > 0 {
-		return fmt.Errorf("failed: max iterations reached: this symlink graph contains a cycle")
+		err = os.Symlink(link.name, link.path)
+		if err != nil {
+			return fmt.Errorf("failed to unzip symlink: %s", err)
+		}
 	}
 
 	return nil
