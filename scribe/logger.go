b45c4d3c76005372e46c47aa12abd59563aa4651
Forest Eckhardt <feckhardt@pivotal.io>
2021-11-18 17:24:37 +0000

    Uses suggested simplified interface

--- scribe/logger.go
+++ scribe/logger.go
@@ -3,37 +3,51 @@ package scribe
 import (
 	"fmt"
 	"io"
-	"os"
 	"strings"
 )
 
 type Logger struct {
+	writer io.Writer
+	LeveledLogger
+	Debug LeveledLogger
+}
+
+func NewLogger(writer io.Writer) Logger {
+	return Logger{
+		writer:        writer,
+		LeveledLogger: NewLeveledLogger(writer),
+		Debug:         NewLeveledLogger(io.Discard),
+	}
+}
+
+func (l Logger) WithLevel(level string) Logger {
+	switch level {
+	case "DEBUG":
+		return Logger{
+			writer:        l.writer,
+			LeveledLogger: NewLeveledLogger(l.writer),
+			Debug:         NewLeveledLogger(l.writer),
+		}
+	default:
+		return Logger{
+			writer:        l.writer,
+			LeveledLogger: NewLeveledLogger(l.writer),
+			Debug:         NewLeveledLogger(io.Discard),
+		}
+	}
+}
+
+type LeveledLogger struct {
 	title      io.Writer
 	process    io.Writer
 	subprocess io.Writer
 	action     io.Writer
 	detail     io.Writer
 	subdetail  io.Writer
-
-	debug bool
-	info  bool
 }
 
-type infoLogger struct {
-	logger Logger
-
-	debug bool
-	info  bool
-}
-
-type debugLogger struct {
-	logger Logger
-
-	debug bool
-}
-
-func NewLogger(writer io.Writer) Logger {
-	l := Logger{
+func NewLeveledLogger(writer io.Writer) LeveledLogger {
+	return LeveledLogger{
 		title:      NewWriter(writer),
 		process:    NewWriter(writer, WithIndent(1)),
 		subprocess: NewWriter(writer, WithIndent(2)),
@@ -41,149 +55,37 @@ func NewLogger(writer io.Writer) Logger {
 		detail:     NewWriter(writer, WithIndent(4)),
 		subdetail:  NewWriter(writer, WithIndent(5)),
 	}
-
-	if level, ok := os.LookupEnv("BP_LOG_LEVEL"); ok {
-		switch level {
-		case "INFO":
-			l.info = true
-		case "DEBUG":
-			l.debug = true
-		}
-	}
-
-	return l
 }
 
-func (l Logger) Title(format string, v ...interface{}) {
+func (l LeveledLogger) Title(format string, v ...interface{}) {
 	l.printf(l.title, format, v...)
 }
 
-func (l Logger) Process(format string, v ...interface{}) {
+func (l LeveledLogger) Process(format string, v ...interface{}) {
 	l.printf(l.process, format, v...)
 }
 
-func (l Logger) Subprocess(format string, v ...interface{}) {
+func (l LeveledLogger) Subprocess(format string, v ...interface{}) {
 	l.printf(l.subprocess, format, v...)
 }
 
-func (l Logger) Action(format string, v ...interface{}) {
+func (l LeveledLogger) Action(format string, v ...interface{}) {
 	l.printf(l.action, format, v...)
 }
 
-func (l Logger) Detail(format string, v ...interface{}) {
+func (l LeveledLogger) Detail(format string, v ...interface{}) {
 	l.printf(l.detail, format, v...)
 }
 
-func (l Logger) Subdetail(format string, v ...interface{}) {
+func (l LeveledLogger) Subdetail(format string, v ...interface{}) {
 	l.printf(l.subdetail, format, v...)
 }
 
-func (l Logger) Break() {
+func (l LeveledLogger) Break() {
 	l.printf(l.title, "\n")
 }
 
-func (l Logger) Info() infoLogger {
-	return infoLogger{
-		logger: l,
-
-		debug: l.debug,
-		info:  l.info,
-	}
-}
-
-func (il infoLogger) Title(format string, v ...interface{}) {
-	if il.info || il.debug {
-		il.logger.Title(format, v...)
-	}
-}
-
-func (il infoLogger) Process(format string, v ...interface{}) {
-	if il.info || il.debug {
-		il.logger.Process(format, v...)
-	}
-}
-
-func (il infoLogger) Subprocess(format string, v ...interface{}) {
-	if il.info || il.debug {
-		il.logger.Subprocess(format, v...)
-	}
-}
-
-func (il infoLogger) Action(format string, v ...interface{}) {
-	if il.info || il.debug {
-		il.logger.Action(format, v...)
-	}
-}
-
-func (il infoLogger) Detail(format string, v ...interface{}) {
-	if il.info || il.debug {
-		il.logger.Detail(format, v...)
-	}
-}
-
-func (il infoLogger) Subdetail(format string, v ...interface{}) {
-	if il.info || il.debug {
-		il.logger.Subdetail(format, v...)
-	}
-}
-
-func (il infoLogger) Break() {
-	if il.info || il.debug {
-		il.logger.Break()
-	}
-}
-
-func (l Logger) Debug() debugLogger {
-	return debugLogger{
-		logger: l,
-
-		debug: l.debug,
-	}
-}
-
-func (dl debugLogger) Title(format string, v ...interface{}) {
-	if dl.debug {
-		dl.logger.Title(format, v...)
-	}
-}
-
-func (dl debugLogger) Process(format string, v ...interface{}) {
-	if dl.debug {
-		dl.logger.Process(format, v...)
-	}
-}
-
-func (dl debugLogger) Subprocess(format string, v ...interface{}) {
-	if dl.debug {
-		dl.logger.Subprocess(format, v...)
-	}
-}
-
-func (dl debugLogger) Action(format string, v ...interface{}) {
-	if dl.debug {
-		dl.logger.Action(format, v...)
-	}
-}
-
-func (dl debugLogger) Detail(format string, v ...interface{}) {
-	if dl.debug {
-		dl.logger.Detail(format, v...)
-	}
-}
-
-func (dl debugLogger) Subdetail(format string, v ...interface{}) {
-	if dl.debug {
-		dl.logger.Subdetail(format, v...)
-	}
-}
-
-func (dl debugLogger) Break() {
-	if dl.debug {
-		dl.logger.Break()
-	}
-}
-
-func (l Logger) printf(writer io.Writer, format string, v ...interface{}) {
+func (l LeveledLogger) printf(writer io.Writer, format string, v ...interface{}) {
 	if !strings.HasSuffix(format, "\n") {
 		format = format + "\n"
 	}
--- scribe/logger_test.go
+++ scribe/logger_test.go
@@ -2,7 +2,6 @@ package scribe_test
 
 import (
 	"bytes"
-	"os"
 	"testing"
 
 	"github.com/paketo-buildpacks/packit/scribe"
@@ -74,92 +73,8 @@ func testLogger(t *testing.T, context spec.G, it spec.S) {
 		})
 	})
 
-	context("Info", func() {
-		context("when BP_LOG_LEVEL is not set to INFO", func() {
-			it("does not print info", func() {
-				logger.Title("some-%s", "title")
-				logger.Process("some-%s", "process")
-				logger.Subprocess("some-%s", "subprocess")
-				logger.Action("some-%s", "action")
-				logger.Detail("some-%s", "detail")
-				logger.Subdetail("some-%s", "subdetail")
-				logger.Break()
-
-				logger.Info().Title("some-info-%s", "title")
-				logger.Info().Process("some-info-%s", "process")
-				logger.Info().Subprocess("some-info-%s", "subprocess")
-				logger.Info().Action("some-info-%s", "action")
-				logger.Info().Detail("some-info-%s", "detail")
-				logger.Info().Subdetail("some-info-%s", "subdetail")
-				logger.Info().Break()
-				Expect(buffer.String()).To(ContainLines(
-					"some-title",
-					"  some-process",
-					"    some-subprocess",
-					"      some-action",
-					"        some-detail",
-					"          some-subdetail",
-					"",
-				))
-
-				Expect(buffer.String()).NotTo(ContainLines(
-					"some-info-title",
-					"  some-info-process",
-					"    some-info-subprocess",
-					"      some-info-action",
-					"        some-info-detail",
-					"          some-info-subdetail",
-					"",
-				))
-			})
-		})
-
-		context("when BP_LOG_LEVEL is set to INFO", func() {
-			var infoLogger scribe.Logger
-
-			it.Before(func() {
-				Expect(os.Setenv("BP_LOG_LEVEL", "INFO")).To(Succeed())
-				infoLogger = scribe.NewLogger(buffer)
-			})
-
-			it("does print info", func() {
-				infoLogger.Title("some-%s", "title")
-				infoLogger.Process("some-%s", "process")
-				infoLogger.Subprocess("some-%s", "subprocess")
-				infoLogger.Action("some-%s", "action")
-				infoLogger.Detail("some-%s", "detail")
-				infoLogger.Subdetail("some-%s", "subdetail")
-				infoLogger.Break()
-
-				infoLogger.Info().Title("some-info-%s", "title")
-				infoLogger.Info().Process("some-info-%s", "process")
-				infoLogger.Info().Subprocess("some-info-%s", "subprocess")
-				infoLogger.Info().Action("some-info-%s", "action")
-				infoLogger.Info().Detail("some-info-%s", "detail")
-				infoLogger.Info().Subdetail("some-info-%s", "subdetail")
-				infoLogger.Info().Break()
-				Expect(buffer.String()).To(ContainLines(
-					"some-title",
-					"  some-process",
-					"    some-subprocess",
-					"      some-action",
-					"        some-detail",
-					"          some-subdetail",
-					"",
-					"some-info-title",
-					"  some-info-process",
-					"    some-info-subprocess",
-					"      some-info-action",
-					"        some-info-detail",
-					"          some-info-subdetail",
-					"",
-				))
-			})
-		}, spec.Sequential())
-	})
-
 	context("Debug", func() {
-		context("when BP_LOG_LEVEL is not set to DEBUG", func() {
+		context("when Log Level is not set to DEBUG", func() {
 			it("does not print info", func() {
 				logger.Title("some-%s", "title")
 				logger.Process("some-%s", "process")
@@ -169,21 +84,13 @@ func testLogger(t *testing.T, context spec.G, it spec.S) {
 				logger.Subdetail("some-%s", "subdetail")
 				logger.Break()
 
-				logger.Info().Title("some-info-%s", "title")
-				logger.Info().Process("some-info-%s", "process")
-				logger.Info().Subprocess("some-info-%s", "subprocess")
-				logger.Info().Action("some-info-%s", "action")
-				logger.Info().Detail("some-info-%s", "detail")
-				logger.Info().Subdetail("some-info-%s", "subdetail")
-				logger.Info().Break()
-
-				logger.Debug().Title("some-debug-%s", "title")
-				logger.Debug().Process("some-debug-%s", "process")
-				logger.Debug().Subprocess("some-debug-%s", "subprocess")
-				logger.Debug().Action("some-debug-%s", "action")
-				logger.Debug().Detail("some-debug-%s", "detail")
-				logger.Debug().Subdetail("some-debug-%s", "subdetail")
-				logger.Debug().Break()
+				logger.Debug.Title("some-debug-%s", "title")
+				logger.Debug.Process("some-debug-%s", "process")
+				logger.Debug.Subprocess("some-debug-%s", "subprocess")
+				logger.Debug.Action("some-debug-%s", "action")
+				logger.Debug.Detail("some-debug-%s", "detail")
+				logger.Debug.Subdetail("some-debug-%s", "subdetail")
+				logger.Debug.Break()
 				Expect(buffer.String()).To(ContainLines(
 					"some-title",
 					"  some-process",
@@ -195,13 +102,6 @@ func testLogger(t *testing.T, context spec.G, it spec.S) {
 				))
 
 				Expect(buffer.String()).NotTo(ContainLines(
-					"some-info-title",
-					"  some-info-process",
-					"    some-info-subprocess",
-					"      some-info-action",
-					"        some-info-detail",
-					"          some-info-subdetail",
-					"",
 					"some-debug-title",
 					"  some-debug-process",
 					"    some-debug-subprocess",
@@ -213,12 +113,11 @@ func testLogger(t *testing.T, context spec.G, it spec.S) {
 			})
 		})
 
-		context("when BP_LOG_LEVEL is set to DEBUG", func() {
+		context("when log level set to DEBUG", func() {
 			var debugLogger scribe.Logger
 
 			it.Before(func() {
-				Expect(os.Setenv("BP_LOG_LEVEL", "DEBUG")).To(Succeed())
-				debugLogger = scribe.NewLogger(buffer)
+				debugLogger = scribe.NewLogger(buffer).WithLevel("DEBUG")
 			})
 
 			it("does print info", func() {
@@ -230,21 +129,13 @@ func testLogger(t *testing.T, context spec.G, it spec.S) {
 				debugLogger.Subdetail("some-%s", "subdetail")
 				debugLogger.Break()
 
-				debugLogger.Info().Title("some-info-%s", "title")
-				debugLogger.Info().Process("some-info-%s", "process")
-				debugLogger.Info().Subprocess("some-info-%s", "subprocess")
-				debugLogger.Info().Action("some-info-%s", "action")
-				debugLogger.Info().Detail("some-info-%s", "detail")
-				debugLogger.Info().Subdetail("some-info-%s", "subdetail")
-				debugLogger.Info().Break()
-
-				debugLogger.Debug().Title("some-debug-%s", "title")
-				debugLogger.Debug().Process("some-debug-%s", "process")
-				debugLogger.Debug().Subprocess("some-debug-%s", "subprocess")
-				debugLogger.Debug().Action("some-debug-%s", "action")
-				debugLogger.Debug().Detail("some-debug-%s", "detail")
-				debugLogger.Debug().Subdetail("some-debug-%s", "subdetail")
-				debugLogger.Debug().Break()
+				debugLogger.Debug.Title("some-debug-%s", "title")
+				debugLogger.Debug.Process("some-debug-%s", "process")
+				debugLogger.Debug.Subprocess("some-debug-%s", "subprocess")
+				debugLogger.Debug.Action("some-debug-%s", "action")
+				debugLogger.Debug.Detail("some-debug-%s", "detail")
+				debugLogger.Debug.Subdetail("some-debug-%s", "subdetail")
+				debugLogger.Debug.Break()
 				Expect(buffer.String()).To(ContainLines(
 					"some-title",
 					"  some-process",
@@ -252,13 +143,6 @@ func testLogger(t *testing.T, context spec.G, it spec.S) {
 					"      some-action",
 					"        some-detail",
 					"          some-subdetail",
-					"",
-					"some-info-title",
-					"  some-info-process",
-					"    some-info-subprocess",
-					"      some-info-action",
-					"        some-info-detail",
-					"          some-info-subdetail",
 					"",
 					"some-debug-title",
 					"  some-debug-process",
