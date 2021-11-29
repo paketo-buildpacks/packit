b49ec9c081741f91d85c79132a98de091538251d
Forest Eckhardt <feckhardt@pivotal.io>
2021-11-29 16:03:57 +0000

    Adds examples and top level package description

---
+++ scribe/scribe.go
@@ -0,0 +1,6 @@
+// Package scribe provies a set of interfaces to allow buildpack authors to
+// control their logs on varying levels of granularity. This exposes high level
+// logging functions with packit specific business logic baked into the logging
+// as well as low level functionality that allows an author to build up their
+// own functionality using atomic peices.
+package scribe
