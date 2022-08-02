947c953fb5ae8f9a5c88bcd411d4cc7732f5302e
Frankie Gallina-Jones <frankieg@vmware.com>
2022-08-02 11:06:11 -0400

    Apply suggestions from code review

--- internal/environment_writer.go
+++ internal/environment_writer.go
@@ -24,11 +24,11 @@ func (w EnvironmentWriter) Write(dir string, env map[string]string) error {
 		return err
 	}
 
-	for key, value := range env {
-		// this regex checks that map keys contain valid env var name characters,
-		// per https://pubs.opengroup.org/onlinepubs/9699919799/
-		validEnvVarRegex := regexp.MustCompile(`^[a-zA-Z_]{1,}[a-zA-Z0-9_]*$`)
+	// this regex checks that map keys contain valid env var name characters,
+	// per https://pubs.opengroup.org/onlinepubs/9699919799/
+	validEnvVarRegex := regexp.MustCompile(`^[a-zA-Z_]{1,}[a-zA-Z0-9_]*$`)
 
+	for key, value := range env {
 		parts := strings.SplitN(key, ".", 2)
 		if !validEnvVarRegex.MatchString(parts[0]) {
 			return fmt.Errorf("invalid environment variable name '%s'", parts[0])
