b06851460d37c328a485004e4cc4ecaffc67cf7e
Joshua Casey <joshuatcasey@gmail.com>
2022-09-08 17:28:00 -0500

    Add update-github-config workflow to sync from github-config/library

---
+++ scripts/.util/tools.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+# shellcheck source=SCRIPTDIR/print.sh
+source "$(dirname "${BASH_SOURCE[0]}")/print.sh"
+
+function util::tools::tests::checkfocus() {
+  testout="${1}"
+  if grep -q 'Focused: [1-9]' "${testout}"; then
+    echo "Detected Focused Test(s) - setting exit code to 197"
+    rm "${testout}"
+    util::print::success "** GO Test Succeeded **" 197
+  fi
+  rm "${testout}"
+}
