9a76368d8644fade397918439768f09a694c8a38
Philipp Stehle <philipp.stehle@sap.com>
2022-12-07 17:24:36 +0100

    Addstack extension support
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- run.go
+++ run.go
@@ -26,11 +26,36 @@ func Run(detect DetectFunc, build BuildFunc, options ...Option) {
 	switch phase {
 	case "detect":
 		Detect(detect, options...)
-
 	case "build":
 		Build(build, options...)
-
 	default:
 		config.exitHandler.Error(fmt.Errorf("failed to run buildpack: unknown lifecycle phase %q", phase))
 	}
+
+}
+
+// RunExtension combines the invocation of both generate and detect into a single entry
+// point. Calling Run from an executable with a name matching "generate" or
+// "detect" will result in the matching DetectFunc or GenerateFunc being called.
+func RunExtension(detect DetectFunc, generate GenerateFunc, options ...Option) {
+	config := OptionConfig{
+		exitHandler: internal.NewExitHandler(),
+		args:        os.Args,
+	}
+
+	for _, option := range options {
+		config = option(config)
+	}
+
+	phase := filepath.Base(config.args[0])
+
+	switch phase {
+	case "detect":
+		Detect(detect, options...)
+	case "generate":
+		Generate(generate, options...)
+	default:
+		config.exitHandler.Error(fmt.Errorf("failed to run buildpack: unknown lifecycle phase %q", phase))
+	}
+
 }
