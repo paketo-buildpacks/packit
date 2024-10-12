5e98483dc2093cd476db4843d560eb0f31c2f9f2
bitgully <32452884+bitgully@users.noreply.github.com>
2024-10-12 07:56:00 +0200

    skip-path feature added for dependency mirrors

--- postal/internal/dependency_mirror.go
+++ postal/internal/dependency_mirror.go
@@ -17,8 +17,44 @@ func NewDependencyMirrorResolver(bindingResolver BindingResolver) DependencyMirr
 	}
 }
 
+// Parses a raw mirror string into a map of arguments.
+func getMirrorArgs(mirror string) map[string]string {
+
+	mirrorArgs := map[string]string{
+		"mirror":    mirror,
+		"skip-path": "",
+	}
+
+	// Split mirror string at commas and extract specified arguments.
+	for _, arg := range strings.Split(mirror, ",") {
+		argPair := strings.SplitN(arg, "=", 2)
+		// If a URI is provided without the key 'mirror=', still treat it as the 'mirror' argument.
+		// This addresses backwards compatibility and user experience as most mirrors won't need any additional arguments.
+		if len(argPair) == 1 && (strings.HasPrefix(argPair[0], "https") || strings.HasPrefix(argPair[0], "file")) {
+			mirrorArgs["mirror"] = argPair[0]
+		}
+		// Add all provided arguments to key/value map.
+		if len(argPair) == 2 {
+			mirrorArgs[argPair[0]] = argPair[1]
+		}
+	}
+
+	// Unescape mirror arguments to support URL-encoded strings.
+	tmp, err := url.PathUnescape(mirrorArgs["mirror"])
+	if err == nil {
+		mirrorArgs["mirror"] = tmp
+	}
+	tmp, err = url.PathUnescape(mirrorArgs["skip-path"])
+	if err == nil {
+		mirrorArgs["skip-path"] = tmp
+	}
+
+	return mirrorArgs
+}
+
 func formatAndVerifyMirror(mirror, uri string) (string, error) {
-	mirrorURL, err := url.Parse(mirror)
+	mirrorArgs := getMirrorArgs(mirror)
+	mirrorURL, err := url.Parse(mirrorArgs["mirror"])
 	if err != nil {
 		return "", err
 	}
@@ -32,7 +68,7 @@ func formatAndVerifyMirror(mirror, uri string) (string, error) {
 		return "", fmt.Errorf("invalid mirror scheme")
 	}
 
-	mirrorURL.Path = strings.Replace(mirrorURL.Path, "{originalHost}", uriURL.Hostname(), 1) + uriURL.Path
+	mirrorURL.Path = strings.Replace(mirrorURL.Path, "{originalHost}", uriURL.Hostname(), 1) + strings.Replace(uriURL.Path, mirrorArgs["skip-path"], "", 1)
 	return mirrorURL.String(), nil
 }
 
